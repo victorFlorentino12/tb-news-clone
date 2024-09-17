54a3f022f7d06e85252bf73ac9f32e60de0d10a7
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2024-09-17 20:37:50 +0000

    fear: add page of contruction

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
 function home() {
-  return console.log("oi");
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ fontFamily: "cursive" }}>
+        Está pagina está em construção por:
+        <a href="https://github.com/victorFlorentino12">Victor Florentino</a>
+      </h1>
+    </div>
+  );
 }
 export default home;
