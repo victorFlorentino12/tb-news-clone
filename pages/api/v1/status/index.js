b8152ce1cd5dcfcc9ff64bcfe546bcb99a8db7fa
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2024-09-23 16:49:14 +0000

    chore: add databese.js and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const query = await database.query("SELECT 1 + 1 as sun");
+  console.log(query);
   return response
     .status(200)
     .json({ mensagem: "oi isso é a pagina de status" });
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,5 +1,4 @@
 test("ensure route status it is working properly", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response);
   expect(response.status).toBe(200);
 });
