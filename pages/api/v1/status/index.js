7a4df600bfd16bc1d69c79f2d1cc33893e0a8605
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2024-09-24 16:46:00 +0000

    feat: take status of database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,25 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const query = await database.query("SELECT 1 + 1 as sun");
-  console.log(query);
-  return response
-    .status(200)
-    .json({ mensagem: "oi isso é a pagina de status" });
+  const databaseVersion = await database.query("SELECT version()");
+  const databaseVersionShort = databaseVersion.rows[0].version.slice(0, 14);
+
+  const databaseMaxConnection = await database.query("SHOW max_connections");
+  const databaseMaxConnectionNumber = Number(
+    databaseMaxConnection.rows[0].max_connections,
+  );
+
+  const databaseUseConection = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity",
+  );
+  const databaseUseConectionNumber = Number(databaseUseConection.rows[0].count);
+
+  const updateAt = new Date().toISOString();
+  return response.status(200).json({
+    update_at: updateAt,
+    version: databaseVersionShort,
+    max_connection: databaseMaxConnectionNumber,
+    used_connection: databaseUseConectionNumber,
+  });
 }
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,9 @@
 test("ensure route status it is working properly", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  const dataStub = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(dataStub);
 });
